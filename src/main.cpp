b004715c30b0c5b30c557a098a35fc777f108824
Eva Pace <eba.pachi@gmail.com>
2024-10-02 20:59:34 -0300

    raylib template init

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <raylib.h>
+
+int main(void) {
+#ifndef DEBUG
+    SetTraceLogLevel(LOG_ERROR);
+#endif
+
+    InitWindow(800, 450, "game");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("game content here.", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
